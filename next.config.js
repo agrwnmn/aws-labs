3cb2e62e75bfab4c63d24ff905d731c383d8a593
agrwnmn <agrwnmn@gmail.com>
2024-09-19 23:27:08 -0400

    Update GitHub Actions workflow and project configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/aws-labs',
-    images: {
-      unoptimized: true,
-    },
-  }
-  
-  module.exports = nextConfig
+  output: 'export',
+  basePath: '/aws-labs',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
